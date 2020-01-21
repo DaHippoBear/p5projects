cede13c6e4408a99ac009a380a1d255ce82b9659
dd <ddddddd>
2020-01-21 15:19:21 -0800

    d

--- control/control.h
+++ control/control.h
@@ -5,12 +5,6 @@
 //  Created by Sengupta, Dhruv on 1/14/20.
 //  Copyright © 2020 Sengupta, Dhruv. All rights reserved.
 //
-char* piglatin (char word[] );
-char* shrthnd (char word[] );
-char array[100];
-char pigLatin[100];
-char shorthand[100];
-void clearPigBuffer(char word[]);
-char* shorty(char* input);
+
 char *pig(char* input);
 char *shorty(char* input);
