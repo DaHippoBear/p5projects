884f20e5a87dedf91d8634d1adafee807e9f6384
DaHippoBear <30704584+DaHippoBear@users.noreply.github.com>
2020-01-23 21:41:22 -0800

    Add files via upload

---
+++ golfwang.h
@@ -0,0 +1,19 @@
+ #import <UIKit/UIKit.h>
+#define speedScale 0.20
+#define speedDamping 0.90 // friction rate
+#define stopSpeed 5.0
+@interface GolfController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *hole;
+@property (strong, nonatomic) IBOutlet UIImageView *ball;
+@property (strong, nonatomic) IBOutlet UIImageView *wall;
+@property (strong, nonatomic) IBOutlet UIImageView *wall2;
+@property (strong, nonatomic) IBOutlet UIImageView *wall3;
+@property (strong, nonatomic) IBOutlet UIImageView *portal;
+@property (strong, nonatomic) IBOutlet UIImageView *portal2;
+// These values simulate speed and friction
+@property (nonatomic) CGPoint firstPoint;
+@property (nonatomic) CGPoint lastPoint;
+@property (nonatomic) float ballVelocityX;
+@property (nonatomic) float ballVelocityY;
+@property (strong, nonatomic) NSTimer *gameTimer;
+@end
