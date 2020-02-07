1cb61d197cd0e5cac1af17a422f0e90b62beb656
DaHippoBear <30704584+DaHippoBear@users.noreply.github.com>
2020-02-06 20:24:27 -0800

    Update blackjack.c

--- control/blackjack.c
+++ control/blackjack.c
@@ -3,21 +3,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int carddealt;
+int* cardtotal;
+int* initialcard;
 
-int blackjack() {
+int* dealcard;
+
+int* initialcardpick(int* initialcard) {
     int cards[52] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
     
     int randomIndex = rand() % 52;
-    int randomValue = cards[randomIndex];
-    int startcard = randomValue;
-    char* ans;
-    printf("hit (h) or stand (s)");
-    scanf("%c", ans);
-   
-    if (ans = 'h') {
+    initialcard = cards[randomIndex];
+     return initialcard;
+    
+}
+
+int* dealcardpick(int* dealcard) {
+    int cardsdeal[52] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
+    
+    int randomIndexdeal = rand() % 52;
+    int randomValuedeal = cardsdeal[randomIndexdeal];
+    return dealcard;
+}
+
+int* totalcard(int* cardtotal, int* initialcard, int* dealcard) {
+    int cardsdealadd[52] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
+    
+    if (dealcard != initialcard) {
         
+    cardtotal = intialcard + randomValuedeal;
+   
     }
     
-    return carddealt;
+    else {
+        dealcard--;
+    }
+    return cardtotal;
 }
